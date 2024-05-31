5457a0b436d0a06a04d53a272fbf88d2d0ea6d9f
Chugreev Kirill <wern587@gmail.com>
2024-05-31 19:22:27 +0300

    Добавьте файлы проекта.

---
+++ memory-caching-api/Controllers/DataController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System.Reflection;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace memory_caching_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        private readonly IDataService _dataService;
+
+        public DataController(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        // GET api/<DataController>/5
+        [HttpGet("{key}")]
+        public IActionResult Get(string key)
+        {
+            if(string.IsNullOrEmpty(key))
+            {
+                return BadRequest();
+            }
+
+            var data = _dataService.Get(key);
+
+            return data == null ? NotFound() : new OkObjectResult(data);
+        }
+
+        // POST api/<DataController>
+        [HttpPost]
+        public IActionResult Post([FromBody] DataModel model)
+        {
+            if (model == null)
+            {
+                return BadRequest();
+            }
+
+            _dataService.Upsert(model.Key, model.Value);
+
+            return Ok();
+        }
+
+        // PUT api/<DataController>/5
+        [HttpPut()]
+        public IActionResult Put([FromBody] DataModel model)
+        {
+            if (model == null)
+            {
+                return BadRequest();
+            }
+
+            _dataService.Upsert(model.Key, model.Value, TimeSpan.FromSeconds(20));
+
+            return Ok();
+        }
+
+        // DELETE api/<DataController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                return BadRequest();
+            }
+
+            _dataService.Delete(key);
+
+            return Ok();
+        }
+    }
+}
---
+++ memory-caching-api/DataService.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace memory_caching_api
+{
+    public class DataService: IDataService
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public DataService(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public string? Get(string key)
+        {
+            var found = _memoryCache.TryGetValue(key, out var value);
+            return found ? value.ToString() : null;
+        }
+
+
+        public void Upsert(string key, string value)
+        {
+            try
+            {
+                var s = _memoryCache.Set(key, value);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        
+        public void Upsert(string key, string value, TimeSpan expiration)
+        {
+            try
+            {
+                var s = _memoryCache.Set(key, value, expiration);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        public void Delete(string key)
+        {
+            try
+            {
+                _memoryCache.Remove(key);
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+        }
+    }
+    public interface IDataService
+    {
+        string? Get(string key);
+
+   
+        void Upsert(string key, string value);
+
+        void Upsert(string key, string value, TimeSpan expiration);
+
+        void Delete(string key);
+    }
+}
